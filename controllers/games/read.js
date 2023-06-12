00d55dcf4995023112e2de0c6dcbaee8c9788f8e
DiegoCuellar777 <112589821+DiegoCuellar777@users.noreply.github.com>
2023-06-12 13:21:29 -0500

    Read con company name (#9)

--- controllers/games/read.js
+++ controllers/games/read.js
@@ -1,9 +1,11 @@
 import Game from "../../models/Game.js"
+import Company from "../../models/Company.js"
+
 
 let read = async(req, res, next) => { // la funcion controladora debe ser asincrona para poder esperar la respuesta de mongo 
   try                             //utilizo try cath para intentar algo y agarrar los errores q puedan surgir 
   {
-  let all = await Game.find()       //utilizo find para buscaR todos los recursos del modelo que es Game
+  let all = await Game.find.populate('company_id', 'name')()       //utilizo find para buscaR todos los recursos del modelo que es Game
   return res.status(200)                   //configuro la respuesta que le tengo que enviar al cliente (front)
     .json({
         Game: all
