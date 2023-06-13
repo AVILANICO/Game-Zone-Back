6813da6d1787de7f74dfc6ba44152b732cdbcbbb
Luis Hernandez <lic.luish05@gmail.com>
2023-06-13 11:02:17 -0500

    schemas y middlewares

--- routes/cart.js
+++ routes/cart.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import passport from 'passport';
 import contoller from '../controllers/carrito.js';
+import validator from '../middlewares/validator.js'
+import cart_update from '../schemas/cartUpdate.js';
+import cartExists from '../middlewares/cartExists.js';
 
 
-const { create, getall, destroyOne } = contoller
+const { create, getall, update, destroyOne, destroyAll } = contoller
 
 
 const router = express.Router();
 
-router.post('/:id', passport.authenticate('jwt', {session: false}),create);
+router.post('/:id', passport.authenticate('jwt', {session: false}),cartExists,create);
 router.get("/", passport.authenticate('jwt', {session:false}), getall);
+router.put('/:id', passport.authenticate('jwt',{ session: false}), validator(cart_update), update )
 router.delete("/:id", passport.authenticate('jwt', {session:false}), destroyOne);
+router.delete('/', passport.authenticate('jwt', { session: false}), destroyAll )
 // router.post('/:id',create);
 
 
