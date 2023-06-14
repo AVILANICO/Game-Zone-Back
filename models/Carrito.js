1ac199ad109ef224b2cc3ac6da605b51477b9071
Luis Hernandez <lic.luish05@gmail.com>
2023-06-13 20:23:40 -0500

    carrito

--- models/Carrito.js
+++ models/Carrito.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 let schema = new mongoose.Schema({
 
   user_id: {type: mongoose.Types.ObjectId, ref: 'users', required: true},
+  product_id: {type: mongoose.Types.ObjectId, ref: 'games', required: true},
   title: {type: String, required: true},
   cover_photo: {type: String, required: false},
   category_id:{type: mongoose.Types.ObjectId,ref: 'categories', required: true},
