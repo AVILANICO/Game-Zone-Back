5c9e3317779b0e9b41ee4b013fdbfab64d62db7e
Luis Hernandez <lic.luish05@gmail.com>
2023-06-15 14:35:49 -0500

    mercado pago

--- controllers/payments/payments.js
+++ controllers/payments/payments.js
@@ -1,30 +1,39 @@
 import mercadopago from 'mercadopago';
+import nodemailer from 'nodemailer'
 
 
 const payments = async (req, res, next) => {
+  let { unit_price, } = req.body
   try {
     mercadopago.configure({ access_token: process.env.ACCESS_TOKEN })
     
     const preference = {
       items: [
         {
-          title: "Donation",
-          unit_price: Number(req.body.unit_price),
+          title:"Juegos: " ,
+          unit_price: unit_price,
           quantity: 1,
+          currency_id: 'USD',
         },
       ],
-      back_urls: {
-        success: 'http://localhost:5173/',
-        failure: 'http://localhost:5173/',
-        pending: '',
-      },
+        back_urls: {
+          success: 'http://localhost:5173/',
+          failure: 'http://localhost:5173/',
+          pending: '',
+        },
+        auto_return: "approved",
     };
 
     const response = await mercadopago.preferences.create(preference);
-    res.status(201).json({ preferenceId: response.body.id , message :"succes payment" });
+    console.log(response);
+    res.status(200).json({ 
+      preferenceId: response.body.id , message :"succes payment"
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Oops! An error occurred while generating the payment.' });
+    res.status(500).json({ 
+      error: 'Oops! An error occurred while generating the payment.' 
+    });
   }
 };
 
