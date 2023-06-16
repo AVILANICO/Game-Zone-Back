2f265c81735a12b3a7dd10f7266474f9aaf15381
pikiavila <wowaura833@gmail.com>
2023-06-16 14:55:35 -0300

    todo listo

--- controllers/payments/payments.js
+++ controllers/payments/payments.js
@@ -25,7 +25,7 @@ const payments = async (req, res, next) => {
       to: user.email,
       subject: "Thank you for your purchase! Below we will show you the details of your order.",
       html: `<div  style=" text-align:center;" > 
-      <img  style="width: 100%; " src="http://localhost:3000/static/media/logo.69cd86d90139cc3bb574.png" alt="photo" /> 
+      <img  style="width: 50%; " src="https://crypto.news/app/uploads/2021/09/GameZone-Unlocks-Access-To-Cross-Chain-Blockchain-Gaming.jpg" alt="photo" /> 
       <h3 style="font-size:20px; text-align:center">Thank you for your purchase!</h3>
       <p style="font-size:16px; text-align:center">Below we will show you the details of your order:</p>
       
